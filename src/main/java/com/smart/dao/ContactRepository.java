86a9e3610584da30098a652d656c3188822875e7
YASH-PRAJAPATI-4903 <114235226+YASH-PRAJAPATI-4903@users.noreply.github.com>
2024-06-28 17:08:25 +0530

    search function
    new search function add in our project.

--- src/main/java/com/smart/dao/ContactRepository.java
+++ src/main/java/com/smart/dao/ContactRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.smart.entities.Contact;
+import com.smart.entities.User;
 
 public interface ContactRepository extends JpaRepository<Contact, Integer>{
 	
@@ -16,5 +17,6 @@ public interface ContactRepository extends JpaRepository<Contact, Integer>{
 	@Query("from Contact as c where c.user.id =:userId")
 	public Page<Contact> findContactsByUser(@Param("userId") int userId, Pageable pageable);
 	
-	
+	//search
+	public List<Contact> findByNameContainingAndUser(String name, User user);
 }
